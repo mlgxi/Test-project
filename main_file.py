dbd5dfac31af47127a607df2e58bf42598f094ad
iZelikov <ivan@zelikov.name>
2025-06-14 12:42:30 +0300

    feat: add "Bulls and cows" game

--- main_file.py
+++ main_file.py
@@ -1,12 +1,15 @@
 from games import guess_num
+from games import bulls_and_cows
 
 def game():
     print('1. Guess a number',
-          '2.',
+          '2. Bulls and cows',
           '...',sep='\n')
     num = int(input('Choose a num for game: '))
     if num == 1:
         guess_num.numbers()
+    elif num == 2:
+        bulls_and_cows.start()
 
 if __name__ == '__main__':
     game()
