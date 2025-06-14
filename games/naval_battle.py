7b459c5fa7d02267cbe8b7b1703af608da862a53
mlgx <ivanovadasha.2011@mail.ru>
2025-06-14 15:46:02 +0400

    feat: to start naval battle

---
+++ games/naval_battle.py
@@ -0,0 +1,5 @@
+
+
+def navalBattle():
+    '''Морской бой'''
+    print('Welcome to game "naval battle!"')
--- main_file.py
+++ main_file.py
@@ -1,15 +1,17 @@
-from games import guess_num
-from games import bulls_and_cows
+from games import guess_num, bulls_and_cows, naval_battle
 
 def game():
     print('1. Guess a number',
           '2. Bulls and cows',
+          '3. naval battle'
           '...',sep='\n')
     num = int(input('Choose a num for game: '))
     if num == 1:
         guess_num.numbers()
     elif num == 2:
         bulls_and_cows.start()
+    elif num == 3:
+        naval_battle.navalBattle()
 
 if __name__ == '__main__':
     game()
