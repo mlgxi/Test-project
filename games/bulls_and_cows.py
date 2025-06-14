bccabd0deb22946af105343a999f0b35e7b2ef6c
iZelikov <ivan@zelikov.name>
2025-06-14 13:42:20 +0300

    feat: create BullGame class

--- games/bulls_and_cows.py
+++ games/bulls_and_cows.py
@@ -1,2 +1,13 @@
+class BullGame:
+    def __init__(self, numbers_count):
+        self.numbers_count = numbers_count
+        pass
+    def run(self):
+        pass
+
 def start():
+    """Начинает игру "Быки и коровы", позволяя выбрать длину цифровой последовательности для угадывания"""
     print('Начинаем игру "Быки и коровы"')
+    numbers_count = input('Сколько цифр вы хотите отгадать? (5-10): ')
+    bg = BullGame(numbers_count)
+    bg.run()
