4cbfaa9c1d52c43fcce0bb4009c91f787fcc2d94
mlgx <ivanovadasha.2011@mail.ru>
2025-06-14 08:46:22 +0400

    feat: to continue a new game

--- games/guess_num.py
+++ games/guess_num.py
@@ -5,6 +5,14 @@ def numbers():
     num1  = int(input('come up with the beginning of the range: '))
     num2 = int(input('come up with the end of the range: '))
     random_num = random.randint(num1,num2)
-    print('great! I imagined a number, try to guess it!')
+    attempt = 0
+    points = 0
+    try_guess = int(input('great! I imagined a number, try to guess it!'))
+    if try_guess == random_num:
+        print(f'Ur guess, {try_guess} is correct! Attempts: {attempt}')
+    else:
+        middle = num2 // 2
+        while not try_guess == random_num:
+            print('sm')
 
 #в разработке
