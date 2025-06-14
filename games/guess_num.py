99191c2f305fb958b6cac1bea138bd01b8e12c44
mlgx <ivanovadasha.2011@mail.ru>
2025-06-14 10:36:58 +0400

    fix: to finish the game

--- games/guess_num.py
+++ games/guess_num.py
@@ -2,7 +2,7 @@ import random
 
 def numbers():
     '''пользователь угадывает загаданное программой число в выбранном им диапазоне'''
-    print('\t Welcome to the game! Ur should guess a number what I\'ll imagine! \n')
+    print('\n\t Welcome to the game! Ur should guess a number what I\'ll imagine! \n')
     points = 0
     def inner():
         while True:
@@ -11,31 +11,27 @@ def numbers():
             random_num = random.randint(minn, maxx)
             nonlocal points
             attempt = 0
-            try_guess = int(input('great! I imagined a number, try to guess it! \n\t'))
+            try_guess = int(input('\n great! I imagined a number, try to guess it! \n\t'))
 
             while True:
                 if try_guess > random_num:
-                    try_guess = int(input('your number is too big, try guess again: '))
+                    try_guess = int(input('your number is too big, try guess again: \n\t'))
                     attempt += 1
                 elif try_guess < random_num:
-                    try_guess = int(input('your number is too small, try guess again: '))
+                    try_guess = int(input('your number is too small, try guess again: \n\t'))
                     attempt += 1
                 else:
-                    print(f'Ur guess, {try_guess} is correct!')
+                    print(f'Ur guess, {try_guess} is correct!\n')
                     attempt += 1
                     break
             if attempt < 4: points += 3
             elif 4 <= attempt < 7: points += 2
             else: points += 1
             finish = input(f'The game has finished! Attempts: {attempt}, points: {points} \n'
-                           f'Do you want play again? \n')
-            if finish == 'Yes':
+                           f'Do you want play again? \n\t')
+            if finish == 'Yes' or finish == 'yes':
                 numbers()
-            elif finish == 'No':
+            elif finish == 'No' or finish == 'no':
                 print('Goodbye!')
                 return points
     inner()
-
-
-if not __name__ == '__main__':
-    numbers()
--- main_file.py
+++ main_file.py
@@ -1,9 +1,12 @@
+from games import guess_num
 
 def game():
     print('1. Guess a number',
           '2.',
           '...',sep='\n')
     num = int(input('Choose a num for game: '))
+    if num == 1:
+        guess_num.numbers()
 
 if __name__ == '__main__':
     game()
