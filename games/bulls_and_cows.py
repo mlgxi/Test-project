4347aee3edaae707fbcb51734f2747b3dc4480a2
iZelikov <ivan@zelikov.name>
2025-06-14 16:48:22 +0300

    feat: add clear console commands to "Bulls and cow" game

--- games/bulls_and_cows.py
+++ games/bulls_and_cows.py
@@ -1,14 +1,18 @@
+from utils import *
+
 class BullGame:
     def __init__(self, numbers_count):
         self.numbers_count = numbers_count
         pass
     def run(self):
+        clear()
         while True:
             pass
 
 
 def start():
     """Начинает игру "Быки и коровы", позволяя выбрать длину цифровой последовательности для угадывания"""
+    clear()
     print('Начинаем игру "Быки и коровы"')
     print('В этой игре вам надо разгадать числовую последовательность за минимальное количество попыток. Чем длиннее последовательность, тем сложнее её восстановить.')
     numbers_count = input('Сколько цифр вы хотите отгадать? (5-10): ')
